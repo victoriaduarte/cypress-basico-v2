d7eec42f4566c269d0b66bc52e237db8156a55e3
vique <victoriarduarte@hotmail.com>
2023-09-12 20:43:59 -0300

    Change test case name

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,7 +2,7 @@
 
 describe('Central de Atendimento ao Cliente TAT', function() {  /* Test suite */
 
-    it('verifica o título da aplicação', function() {  /* Test case */
+    it('verify the page title', function() {  /* Test case */
         cy.visit('./src/index.html')  /* Relative path based on cypress.json */
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
