b13cd686f3d4b895f68d32e645cde4f9eb954d42
vique <victoriarduarte@hotmail.com>
2023-09-12 22:15:37 -0300

    Add exercise 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,13 +10,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {  /* Test suite */
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('fill out the required fields and send the form', function() {
+    it('fill out the required fields and send the form', function() {
+        const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
+
         cy.get('#firstName').type('Victoria')
         cy.get('#lastName').type('Duarte')
         cy.get('#email').type('victoria@example.com')
-        cy.get('#open-text-area').type('test')
+        cy.get('#open-text-area').type(longText, {delay: 0})  /* enter the text immediately (no delay in typing) */
         cy.get('button[type="submit"]').click()
-        
+
         cy.get('.success').should('be.visible')
         })
   })
