c57331fab78a2ffd9b504fb18badce17ab745620
vique <victoriarduarte@hotmail.com>
2023-09-12 21:16:48 -0300

    Add exercise

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,9 +2,22 @@
 
 describe('Central de Atendimento ao Cliente TAT', function() {  /* Test suite */
 
-    it('verify the page title', function() {  /* Test case */
+    this.beforeEach(function() {
         cy.visit('./src/index.html')  /* Relative path based on cypress.json */
+    })
+
+    it('verify the page title', function() {  /* Test case */
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
+
+    it.only('fill out the required fields and send the form', function() {
+        cy.get('#firstName').type('Victoria')
+        cy.get('#lastName').type('Duarte')
+        cy.get('#email').type('victoria@example.com')
+        cy.get('#open-text-area').type('test')
+        cy.get('button[type="submit"]').click()
+        
+        cy.get('.success').should('be.visible')
+        })
   })
   
