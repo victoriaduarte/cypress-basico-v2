efe5678c4fb7e2fe6041222864bd7de50f537029
vique <victoriarduarte@hotmail.com>
2023-09-18 11:25:58 -0300

    Add API test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -223,4 +223,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {  /* Test suite *
             .invoke('val', longText)
             .should('have.value', longText)
     })
+
+    it('make an HTTP request', () => {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function (response) {
+                const { status, statusText, body } = response
+                expect(status).to.equal(200);
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+            })
+    })
 })
